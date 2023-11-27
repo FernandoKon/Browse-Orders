18c403acef85d269d1963669bbc6cd008ad57a5b
fernando <fehkon@hotmail.com>
2023-11-27 13:39:40 -0300

    feat Order List

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,15 +1,54 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel",
+    "sap/m/MessageBox",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+    "com/lab2dev/browseorders/model/formatter",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel, ODataModel, MessageBox, Filter, FilterOperator, formatter) {
         "use strict";
 
         return Controller.extend("com.lab2dev.browseorders.controller.Home", {
-            onInit: function () {
+            formatter: formatter,
 
-            }
+            onInit: function () {
+                const oDataModel = new ODataModel("/northwind/northwind.svc/");
+
+                oDataModel.read("/Orders", {
+                    success: (oOrders) => {
+                        console.log(oOrders);
+                        const orders = oOrders.results;
+
+                        console.log(orders);
+
+                        const oModel = new JSONModel(orders);
+                        this.getView().setModel(oModel, 'orders');
+                    },
+                    error: (oError) => {
+                        MessageBox.error("Erro ao carregar os dados.")
+                    }
+                })
+            },
+
+            onSearch: function(oEvent) {
+                const aFilters = [];
+                const sQuery = oEvent.getSource().getValue();
+                if (sQuery && sQuery.length > 0) {
+                    const filter = new Filter("ShipName", FilterOperator.Contains, sQuery);
+                    aFilters.push(filter);
+                }
+
+                // update list binding
+                const oList = this.byId("idList");
+                const oBinding = oList.getBinding("items");
+                oBinding.filter(aFilters);
+                
+            },
+
         });
     });
---
+++ webapp/model/formatter.js
@@ -0,0 +1,61 @@
+sap.ui.define([
+    "sap/ui/core/format/DateFormat"
+ ], function (DateFormat) {
+    "use strict";
+ 
+    return {
+       formatDate: function (oDate) {
+          if (!oDate) {
+             return "";
+          }
+ 
+          var oDateFormat = DateFormat.getDateInstance({
+             style: "medium",
+             pattern: "dd 'de' MMM. 'de' yyyy"
+          });
+ 
+          return oDateFormat.format(oDate);
+       },
+
+       formatDate2: function (oDate) {
+            if (!oDate) {
+            return "";
+            }
+
+            var oDateFormat = DateFormat.getDateInstance({
+            style: "medium",
+            pattern: "dd'/'MM'/'yyyy"
+            });
+
+            return oDateFormat.format(oDate);
+        },
+
+        getStatusText: function (sShippedDate, sOrderDate) {
+            const shippedDate = this.formatDate2(sShippedDate);
+            const orderDate = this.formatDate2(sOrderDate);
+        
+            if (shippedDate === orderDate) {
+                return "In Time";
+            } else if (shippedDate < orderDate ) {
+                return "Urgent";
+            } else if (shippedDate > orderDate) {
+                return "Too Late";
+            } else {
+                return "Invalid Date";
+            }
+        },
+
+       status :  function (sStatus) {
+        if (sStatus === "In Time") {
+            return "Success";
+        } else if (sStatus === "Urgent") {
+            return "Warning";
+        } else if (sStatus === "Too late"){
+            return "Error";
+        } else {
+            return "None";
+        }
+        },
+    };
+ });
+ 
